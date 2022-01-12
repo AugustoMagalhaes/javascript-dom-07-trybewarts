696ecf97c5ecd05f288946e879cc3f98f44dafba
AugustoMagalhaes <augustommg@gmail.com>
2022-01-12 03:40:37 -0300

    R13 - Criando campos de entrada do tipo 'checkbox'; retirando id 'email' repetido no header que confundiu os testes automatizados; adequando o arquivo 'script.js' para aceitaçao do Linter.

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-window.onload = function () {
-    const joinButton = document.querySelector("#button-join");
-    joinButton.addEventListener("click", clickJoinButtonEvent);
-};
-
 function clickJoinButtonEvent() {
-    const emailInput = document.querySelector("#input-email");
-    const passwordInput = document.querySelector("#input-password");
-    if (
-        !(
-            emailInput.value === "tryber@teste.com" &&
-            passwordInput.value === "123456"
-        )
-    ) {
-        alert("Email ou senha inválidos.");
-        return;
-    }
-    alert("Olá, Tryber!");
+  const emailInput = document.querySelector('#input-email');
+  const passwordInput = document.querySelector('#input-password');
+  if (
+    !(emailInput.value === 'tryber@teste.com' && passwordInput.value === '123456')
+  ) {
+    alert('Email ou senha inválidos.');
+    return;
+  }
+  alert('Olá, Tryber!');
 }
+
+function loadPage() {
+  const joinButton = document.querySelector('#button-join');
+  joinButton.addEventListener('click', clickJoinButtonEvent);
+}
+
+window.onload = loadPage;

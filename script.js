f7959126241b13a2b9a00103d752da6e1622928c
Matheus <stvgamer74@gmail.com>
2022-01-13 12:50:14 -0300

    R20: Criando contador de caracteres funcional para o textArea

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 function clickJoinButtonEvent() {
   const emailInput = document.querySelector('#user-email');
   const passwordInput = document.querySelector('#user-password');
-  if (
-    !(emailInput.value === 'tryber@teste.com' && passwordInput.value === '123456')
-  ) {
+  if (!(emailInput.value === 'tryber@teste.com' && passwordInput.value === '123456')) {
     alert('Email ou senha inválidos.');
     return;
   }
   alert('Olá, Tryber!');
 }
 
+function textAreaEvent() {
+  const textArea = document.querySelector("#textarea");
+  const count = document.querySelector("#counter");
+  count.innerHTML = 500 - textArea.value.length;
+}
+
 function loadPage() {
   const joinButton = document.querySelector('#button-join');
   joinButton.addEventListener('click', clickJoinButtonEvent);
+  const textArea = document.querySelector('#textarea');
+  textArea.addEventListener('input', textAreaEvent)
 }
 
 window.onload = loadPage;

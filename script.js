4770fa646a96d071160200c037f3ad05e1ef184f
Matheus <stvgamer74@gmail.com>
2022-01-14 21:47:26 -0300

    R21: Substituindo conteudo do form para a saida com os resultados

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function textAreaEvent() {
 function sendForm() {
   const checkbox = document.getElementById('agreement');
   const submitBtn = document.getElementById('submit-btn');
+  console.log(submitBtn.value);
   if (checkbox.checked === true) {
     submitBtn.disabled = false;
   }
@@ -26,6 +27,85 @@ function sendForm() {
   } else { alert('enviou'); } */
 }
 
+function clearForm(form) {
+  for (let i = form.childNodes.length - 1; i >= 0; i -= 1) {
+    const item = form.childNodes[i];
+    console.log(i, item);
+    item.remove();
+  }
+}
+
+function getFamily() {
+  const family = document.querySelectorAll('[name=family]');
+  let exit = null;
+  for (let i = 0; i < family.length; i += 1) {
+    const key = family[i];
+    if (key.checked) {
+      exit = key;
+    }
+  }
+  return exit;
+}
+
+function getContentBox() {
+  const contentBox = document.querySelectorAll('[name=content-box]');
+  const array = [];
+  for (let i = 0; i < contentBox.length; i += 1) {
+    const key = contentBox[i];
+    if (key.checked) {
+      array.push(key.value);
+    }
+  }
+  return array.toString().replace(/,/g, ', ');
+}
+
+function getRate() {
+  const rate = document.querySelectorAll('[name=rate]');
+  let exit = null;
+  for (let i = 0; i < rate.length; i += 1) {
+    const key = rate[i];
+    if (key.checked) {
+      exit = key;
+    }
+  }
+  return exit;
+}
+
+function getArray() {
+  const name = document.querySelector('[name=input-name]').value;
+  const lastName = document.querySelector('[name=input-lastname]').value;
+  const email = document.querySelector('[name=input-email]').value;
+  const house = document.querySelector('[name=house]').value;
+  const family = getFamily().value;
+  const contentBox = getContentBox();
+  const rate = getRate().value;
+  const textarea = document.querySelector('[name=textarea]').value;
+  const agreement = document.querySelector('[name=agreement]').value;
+  return [name, lastName, email, house, family, contentBox, rate, textarea, agreement];
+}
+
+function appendSpan(father, str) {
+  const span = document.createElement('span');
+  span.innerHTML = str;
+  father.appendChild(span);
+}
+
+function buttonSubmitEvent(event) {
+  event.preventDefault();
+  const form = document.querySelector('#evaluation-form');
+  const array = getArray();
+
+  clearForm(form);
+
+  appendSpan(form, `Nome: ${array[0]} ${array[1]}`);
+  appendSpan(form, `Email: ${array[2]}`);
+  appendSpan(form, `Casa: ${array[3]}`);
+  appendSpan(form, `Família: ${array[4]}`);
+  appendSpan(form, `Matérias: ${array[5]}`);
+  appendSpan(form, `Avaliação: ${array[6]}`);
+  appendSpan(form, `Observações: ${array[7]}`);
+}
+
 function loadPage() {
   const joinButton = document.querySelector('#button-join');
   joinButton.addEventListener('click', clickJoinButtonEvent);
@@ -33,6 +113,8 @@ function loadPage() {
   textArea.addEventListener('input', textAreaEvent);
   const checkbox = document.getElementById('agreement');
   checkbox.addEventListener('click', sendForm);
+  const button = document.getElementById('submit-btn');
+  button.addEventListener('click', buttonSubmitEvent);
 }
 
 window.onload = loadPage;
